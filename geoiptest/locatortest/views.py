55f1409213ac4374d06f6447d132e2aa125cb8a1
t0n <af32tv28>
2014-05-27 19:36:03 +0300

    test of geoip

--- geoiptest/locatortest/views.py
+++ geoiptest/locatortest/views.py
@@ -7,20 +7,42 @@ from django.core.urlresolvers import reverse
 from django.core import serializers
 
 from annoying.decorators import render_to
-from pygeoip import GeoIP
+from django.contrib.gis.geoip import GeoIP
 
 @render_to('site/index.html')
 def home(request):
-    geoip = GeoIP(settings.GEOIP_PATH)
-    client_ip = request.META['REMOTE_ADDR']
-    country = geoip.country_name_by_name(client_ip)
-    record = geoip.record_by_name(client_ip)
-    city = record['city']
-    lat = record['latitude']
-    lon = record['longitude']
+    geoip = GeoIP()
+    client_ip = get_client_ip(request)
+
+    country_record = geoip.country(client_ip)
+    if country_record:
+        country = country_record['country_name']
+    else:
+        country = 'Not defined'
+
+    city_record = geoip.city(client_ip)
+    if city_record:
+        city = city_record['city']
+    else:
+        city = 'Not defined'
+
+    lat_lon_record = geoip.lat_lon(client_ip)
+    if lat_lon_record:
+        (lat, lon) = lat_lon_record
+    else:
+        (lat, lon) = (-1, -1)
     return {
+        'client_ip': client_ip,
         'country': country,
         'city': city,
         'lat': lat,
         'lon': lon,
     }
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
